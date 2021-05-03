522bafdf34c1fcec9419dc1249a601fd70efac53
KarimLa <karimacc001@gmail.com>
2021-05-03 20:24:13 +0000

    3 challenge: running python scripts

---
+++ python_wrangling/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	pwd, err := ioutil.ReadFile("pw.txt")
+	if err != nil {
+		log.Fatal(pwd)
+	}
+
+	pythonPath, err := exec.LookPath("python3")
+	if err != nil {
+		log.Println(err)
+		log.Fatal("install python in your system!")
+	}
+
+	cmd := exec.Command(pythonPath, "ende.py", "-d", "flag.txt.en", string(pwd))
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
