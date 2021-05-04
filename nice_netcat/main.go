c3b033c83724f052963bd5c3ce416dce71fe114f
KarimLa <karimacc001@gmail.com>
2021-05-04 00:40:02 +0000

    task 6: base10 to text

---
+++ nice_netcat/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// TODO(soramon0): read data file and convert
+	// base10 to text to get the falg
+	//
+	// data, err := ioutil.ReadFile("data")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	fmt.Println("picoCTF{g00d_k1tty!_n1c3_k1tty!_3d84edc8}")
+}
