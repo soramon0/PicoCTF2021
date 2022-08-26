a524a19163041f2c5b44a151b9b415918c1063fd
soramon0 <karimacc001@gmail.com>
2022-08-27 00:23:53 +0100

    fix login script

--- overthewire/bandit/login.sh
+++ overthewire/bandit/login.sh
@@ -4,13 +4,14 @@
 files=`ls -v | grep -i "^bandit*"`
 last_file=`echo "$files" | tail -1`
 
-if [ -f "$1" ]; then
-	last_file=$1
-else
-	echo "$1 does not exist."
-	exit 1
+if [ "$1" != "" ]; then
+	if [ -f "$1" ]; then
+		last_file=$1
+	else
+		echo "$1 does not exist."
+		exit 1
+	fi
 fi
-
 
 echo "ssh to $last_file@bandit.labs.overthewire.org"
 sshpass -p `cat "$last_file"` ssh $last_file@bandit.labs.overthewire.org -p 2220
