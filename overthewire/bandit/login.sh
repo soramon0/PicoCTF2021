3b4b3a184857777f26b823e69cfd42640e37cfc8
soramon0 <karimacc001@gmail.com>
2022-08-27 00:08:07 +0100

    configure bandit login script to take custom file name

--- overthewire/bandit/login.sh
+++ overthewire/bandit/login.sh
@@ -4,6 +4,14 @@
 files=`ls -v | grep -i "^bandit*"`
 last_file=`echo "$files" | tail -1`
 
+if [ -f "$1" ]; then
+	last_file=$1
+else
+	echo "$1 does not exist."
+	exit 1
+fi
+
+
 echo "ssh to $last_file@bandit.labs.overthewire.org"
 sshpass -p `cat "$last_file"` ssh $last_file@bandit.labs.overthewire.org -p 2220
  
